1877046362eee4a124551fbcde8786f5611af1b9
saloni-jain-code <sjain102015@gmail.com>
2024-08-06 23:40:47 -0400

    fixed firebase configuration

--- firebase.js
+++ firebase.js
@@ -10,12 +10,12 @@ import {getFirestore} from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  authDomain: "inventory-management-1e624.firebaseapp.com",
+  projectId: "inventory-management-1e624",
+  storageBucket: "inventory-management-1e624.appspot.com",
+  messagingSenderId: "172269333957",
+  appId: "1:172269333957:web:c5f5642d67589904756fac",
+  measurementId: "G-K9CVP1LTQB"
 };
 
 // Initialize Firebase
